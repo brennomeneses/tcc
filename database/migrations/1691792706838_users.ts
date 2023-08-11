b9f7402d151a544269126c51d31443d559000b7b
brennomeneses <brennocorps@gmail.com>
2023-08-11 20:04:13 -0300

    Migrations and database config

---
+++ database/migrations/1691792706838_users.ts
@@ -0,0 +1,29 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.uuid('uuid')
+
+      table.string('name').notNullable()
+      table.string('email').notNullable()
+      table.string('password').notNullable()
+      table.string('role').notNullable()
+
+      table.integer('companyId').unsigned().references('users.id').onDelete('CASCADE')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
